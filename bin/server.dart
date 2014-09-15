0e0ea6930414f5cb20b182996c7fe7d83f04eb05
Julie Ralph <ju.ralph@gmail.com>
2014-09-15 13:05:06 -0700

    Allow the command line server app to serve dart as well as js

--- bin/server.dart
+++ bin/server.dart
@@ -2,7 +2,10 @@ import 'dart:async';
 import 'dart:io';
 import 'package:http_server/http_server.dart';
 
-VirtualDirectory staticFiles = new VirtualDirectory('build/web');
+VirtualDirectory staticFiles = new VirtualDirectory('build/web')
+  ..followLinks = true
+  ..jailRoot = false
+  ..allowDirectoryListing = true;
 
 void _serveRequest (HttpRequest request) {
   print(request.uri.path);
@@ -17,8 +20,19 @@ void _serveRequest (HttpRequest request) {
 }
 
 void main() {
-  HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8095).then((HttpServer server) {
-    print('Server listening at port ${server.port}');
-    server.listen(_serveRequest);
+
+  // All the Process calls here are a hack to fix up the build folder so that it
+  // can serve Dart as well as JavaScript.
+  Process.run('rsync', ['-rl', '--exclude', 'package', 'web/', 'build/web']).then((ProcessResult results) {
+    Process.run('rm', ['-rf', 'build/web/packages']).then((ProcessResult results) {
+      Process.run('ln', ['-s', Directory.current.path + '/packages', 'build/web/packages']).then((ProcessResult results) {
+        Process.run('ln', ['-s', Directory.current.path + '/lib', 'build/web/packages/testapp']).then((ProcessResult results) {
+          HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8095).then((HttpServer server) {
+            print('Server listening at port ${server.port}');
+            server.listen(_serveRequest);
+          });
+        });
+      });
+    });
   });
 }
